e1e5b45b6cbe9e6665e77e3c44db770dc0be11b2
joelmccl <joelmccl@iastate.edu>
2023-05-18 11:45:22 -0500

    Mostly just set up while I look things up for shape manager

--- Shape Manager Assignment/ShapeManager/ShapeManager/Shape.h
+++ Shape Manager Assignment/ShapeManager/ShapeManager/Shape.h
@@ -1,1 +1,20 @@
-#pragma once
+#include <iostream>	//Standard includes again.
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class shape
+{
+public:
+	virtual void setLength(double);
+	virtual void setBredth(double);
+	virtual double calculateArea();
+	virtual void printArea();
+
+	void setName();
+	string getName();
+protected:
+	string name;
+};
---
+++ Shape Manager Assignment/ShapeManager/ShapeManager/main.cpp
@@ -0,0 +1,20 @@
+/*******************************
+Introduction to Programming
+
+Program to demonstrate usage of classes
+
+Programmer: Joel McCleary
+********************************/
+
+#include <iostream>	//Here we have some of the normal included headers
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	
+
+	return 0;
+}
